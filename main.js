7d788773d58a819d6d0f301dc98deae112c4b9ca
Prashanta mondal <iloveprashanta@gmail.com>
2020-11-28 13:38:02 +0530

    update

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 
 var textarea = document.getElementById('pastelinks');
+function openall() {
+    var to_Lower = textarea.value.toLowerCase();
+    var removeSpace = to_Lower.replace(/\s/g, '');
+    var intoobj = removeSpace.split(',');
+    var missingExtn = [];
+    intoobj.forEach(function (link) {
+        if (link.indexOf('.') == -1) {
+            if (link.indexOf('http') == -1) {
+                // http and extension both missing
+                window.open('http://google.com/search?q=' + link); //for basic search
+            } else {
+                // http here but extension missing
+                //window.open('http://google.com/search?q=' + link.substr(7)); // for basic search
+                missingExtn.push(link.substr(7));
+            }
+        }
+        else if (link.indexOf('http') == -1) {
+            // only http missing , extension found
+            window.open('http://' + link);
+        }
+        else {
+            // perfect link
+            window.open(link, '_blank');
+        }
+    })
+    fetch("websitelist.json")
+        .then(response => response.json())
+        .then(website_data => website_data.forEach(function (name) {
+            if (name.website_name.search(missingExtn) != -1) {
+                window.open('http://' + name.website_name, '_blank');
+            }
+        }));
+}
+
 
 function clearr() {
     if (!textarea.value.length == 0) {
@@ -12,22 +46,32 @@ function clearr() {
     }
 }
 
-function openall() {
-    var removeSpace = textarea.value.replace(/\s/g, '');
-    var intoobj = removeSpace.split(',');
-    intoobj.forEach(function (link) {
-        if (link.indexOf('.') == -1) {
-            if (link.indexOf('http') == -1) {
-                window.open('http://google.com/search?q=' + link);
-            } else {
-                window.open('http://google.com/search?q=' + link.substr(7));
-            }
-        }
-        else if (link.indexOf('http') == -1) {
-            window.open('http://' + link);
-        }
-        else {
-            window.open(link, '_blank');
-        }
-    })
+
+function popupImg() {
+    document.getElementById('popup_image').style.display = 'block';
 }
+function closePopup() {
+    document.getElementById('popup_image').style.display = 'none';
+}
+
+
+
+
+//Refreshing page automatically when viewport size change
+// window.addEventListener('resize', function () {
+//     window.location.reload();
+// });
+
+//press enter
+// textarea.addEventListener("keyup", function (e) {
+//     if (e.keyCode === 13) {
+//         e.preventDefault();
+//         document.getElementById('openall').click();
+//     }
+// })
+///
+// if (window.visualViewport.width <= 650) {
+//     textarea.style.width = (window.visualViewport.width - 20) + "px";
+// } else {
+//     textarea.style.width = 630 + "px";
+// }
